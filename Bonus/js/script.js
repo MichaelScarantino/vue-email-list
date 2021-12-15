d32fa326dbb06f8b7f76533cf87b8220cf519a29
MichaelScarantino <scarantino.michael@outlook.it>
2021-12-15 16:08:21 +0100

    Done Bonus

---
+++ Bonus/js/script.js
@@ -0,0 +1,35 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Vue.config.devtools = true;
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            emailArray: [],
+            numberOfEmail: 10,
+        },
+        methods: {
+            getArrayEmail: function(emailArray) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                   const serverGenerateEmail = response.data.response;
+                   this.emailArray.push(serverGenerateEmail);
+                });
+            }
+        },
+        created: function() {
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+            this.getArrayEmail();
+        }
+    }
+);
